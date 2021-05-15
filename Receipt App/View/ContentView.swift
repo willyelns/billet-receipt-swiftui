8509d01331a807c9ea18a18f2dc3772c4408a13c
Will Xavier <willyelnsg.x@gmail.com>
2021-05-15 20:08:33 -0300

    feat: add delete and edit option to the list

--- Receipt App/View/ContentView.swift
+++ Receipt App/View/ContentView.swift
@@ -13,17 +13,28 @@ struct ContentView: View {
     
     @Environment(\.managedObjectContext) var managedObjectContext
     
+    @FetchRequest(entity: Todo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Todo.name, ascending: true)]) var todos: FetchedResults<Todo>
+    
     @State private var showingAddTodoView: Bool = false
     
     // MARK: - BODY
     
     var body: some View {
         NavigationView {
-            List(0 ..< 5) { item in
-                Text("Hello, World")
-            } //: LIST
+            List{
+                ForEach(self.todos, id: \.self) { todo in
+                    HStack {
+                        Text(todo.name ?? "Unknown")
+                        Spacer()
+                        Text(todo.priority ?? "Unknown")
+                    }
+                } //: FOREACH
+                .onDelete(perform: deleteTodo)
+            }//: LIST
             .navigationBarTitle("Todo", displayMode: .inline)
-            .navigationBarItems(trailing:
+            .navigationBarItems(
+                leading: EditButton(),
+                trailing:
                 Button(action: {
                     self.showingAddTodoView.toggle()
                 }) {
@@ -34,6 +45,21 @@ struct ContentView: View {
                 }
             )
         } //: NAVIGATION
+    }
+    
+    // MARK: - FUNCTIONS
+    
+    private func deleteTodo(at offsets: IndexSet) {
+        for index in offsets {
+            let todo = todos[index]
+            managedObjectContext.delete(todo)
+            
+            do {
+                try managedObjectContext.save()
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 
